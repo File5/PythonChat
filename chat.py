25211eefc9e2cc0fefe829df14b8f6c938e04d04
Alexander Zuev <zuev08@gmail.com>
2015-10-21 23:04:37 +0300

    Add new methods and errors handling.
    chat.py
    
    Move creating widgets to new create_gui() method
    Delete trailing spaces in headers (read_settings() method)
    Add close() method which closes connection
    Bind close() to Close button
    Add print_error() method
    Add wrong command handling
    Add do_nothing() method
    Rename available to available_cmds
    
    socketutils.py
    
    Make ClientSocketListener subclass Thread
    Add close() method which closes connection and stops thread

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from _tkinter import TclError
 from socket import gethostname, gethostbyname
 import socketutils
 
@@ -6,10 +7,39 @@ class ChatApplication:
     
     def __init__(self):
         #  creating widgets
+        self.create_gui()
+        
+        #  bing <Return> key to send message method
+        self.root.bind('<Return>', self.send_msg)
+        #  bing Close button to close method
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+        #  setting up receiving and sending socket
+        self.name = gethostbyname(gethostname())
+        self.THISHOST = gethostbyname(gethostname())
+        self.HOST = '127.0.0.1'
+        self.PORT = 1488
+        self.read_settings()
+
+        self.client = socketutils.ClientSocketListener(self.print_msg, self.HOST, self.PORT)
+
+        #  setting available commands
+        self.available_cmds = {'/font': self.set_font,
+                          '/bg': self.set_bg,
+                          '/color': self.set_fg,
+                          '/matrix': self.matrix,
+                          '/clear': self.clear_text,
+                          '/help': self.print_help,
+                          '/name': self.set_name,
+                          '/clients': self.do_nothing,
+                          }
+        
+        self.root.mainloop()
+    
+    def create_gui(self):
         self.root = Tk()
         self.root.geometry("600x450")
         self.root.title("Chat")
-        self.root.bind('<Return>', self.send_msg)
         
         self.scrollbar = Scrollbar(self.root)
         self.scrollbar.pack(side=RIGHT, fill=Y)
@@ -21,6 +51,7 @@ class ChatApplication:
         
         self.scrollbar.config(command=self.text.yview)
         
+        #  bottom frame
         self.send_frame = Frame(self.root)
         self.send_frame.pack(side=BOTTOM, fill=X)
         
@@ -29,30 +60,20 @@ class ChatApplication:
 
         self.button = Button(self.send_frame, text="Send", command=self.send_msg)
         self.button.pack(side=RIGHT)
-
-        #  setting up receiving and sending sockets
-        self.name = gethostbyname(gethostname())
-        self.THISHOST = gethostbyname(gethostname())
-        self.HOST = '127.0.0.1'
-        self.PORT = 1489
-        self.read_settings()
-
-        self.client = socketutils.ClientSocketListener(self.print_msg, self.HOST, self.PORT)
-
-        #  setting available commands
-        self.available = {'/font': self.set_font,
-                          '/bg': self.set_bg,
-                          '/color': self.set_fg,
-                          '/matrix': self.matrix,
-                          '/clear': self.clear_text,
-                          '/help': self.print_help,
-                          '/name': self.set_name,
-                          }
-        
-        self.root.mainloop()
+    
+    def close(self):
+        self.client.close()
+        self.root.destroy()
     
     def print_msg(self, msg):
         msg = str(msg, "cp1251")
+        self.text['state'] = NORMAL
+        self.text.insert(END, msg)
+        self.text.see(END)
+        self.text['state'] = DISABLED
+        
+    def print_error(self, msg):
+        msg = "Error: " + msg + '\n'
         self.text['state'] = NORMAL
         self.text.insert(END, msg)
         self.text.see(END)
@@ -74,10 +95,10 @@ class ChatApplication:
         #  reading settings.ctg and setting variables
         try:
             with open('settings.cfg') as f:
-                headers = {'name': 'Name: ',
-                           'thishost': "This-Host: ",
-                           'host': 'Server-Host: ',
-                           'port': 'Server-Port: '}
+                headers = {'name': 'Name:',
+                           'thishost': "This-Host:",
+                           'host': 'Server-Host:',
+                           'port': 'Server-Port:'}
                 for line in f:
                     if line.startswith(headers['name']):
                         self.name = line[ len(headers['name']) : ].strip()
@@ -91,21 +112,33 @@ class ChatApplication:
             pass
 
     def run_command(self, text):
-        for command in self.available:
-            if text.startswith(command):
-                self.available[command]( text[ len(command) : ].strip() )
+        cmd, sep, args = text.partition(' ')
+        cmd_func = self.available_cmds.get(cmd, None)
+        if cmd_func is None:
+            error = "No such command. Type /help to get list of commands."
+            self.print_error(error)
+        else:
+            cmd_func(args.strip())
 
     def set_font(self, x):
         self.text['font'] = x
         self.edit['font'] = x
         
     def set_bg(self, x):
-        self.text['bg'] = x
-        self.edit['bg'] = x
+        try:
+            self.text['bg'] = x
+            self.edit['bg'] = x
+        except TclError:
+                error = "No such color."
+                self.print_error(error)
         
     def set_fg(self, x):
-        self.text['fg'] = x
-        self.edit['fg'] = x
+        try:
+            self.text['fg'] = x
+            self.edit['fg'] = x
+        except TclError:
+            error = "No such color."
+            self.print_error(error)
 
     def matrix(self, *args):
         self.text['font'] = "Consolas 12"
@@ -122,12 +155,15 @@ class ChatApplication:
         self.text['state'] = DISABLED
 
     def print_help(self, *args):
-        helptext = '\n'.join(self.available) + '\n'
+        helptext = '\n'.join(self.available_cmds) + '\n'
         self.print_msg(bytes(helptext, "cp1251"))
 
     def set_name(self, name):
         self.name = name
         self.print_msg(bytes(name + ": name saved.\n", "cp1251"))
+
+    def do_nothing(self, *args):
+        pass
 
 if __name__ == "__main__":
     application = ChatApplication()
--- socketutils.py
+++ socketutils.py
@@ -1,29 +1,32 @@
 import socket
 from threading import Thread
 
-class ClientSocketListener:
+class ClientSocketListener(Thread):
 
     def __init__(self, update, HOST='127.0.0.1', PORT=1488):
+        Thread.__init__(self)
         self.update = update
         self.HOST = HOST
         self.PORT = PORT
+        self.is_run = True
 
         self.s = socket.socket()
         self.s.connect((self.HOST, self.PORT))
 
-        self.th = Thread(target=self.listen, args=(self,))
-        self.th.start()
+        self.start()
 
     def send(self, data):
         self.s.send(data)
 
-    def listen(self, *args):
-        while True:
+    def run(self, *args):
+        while self.is_run:
             try:
-                data = b''
-                while data == b'':
-                    data = self.s.recv(1024)
+                data = self.s.recv(1024)
+                if data != b'':
                     self.update(data)
             except Exception as e:
-                print(e)
                 break
+    
+    def close(self):
+        self.is_run = False
+        self.s.close()
